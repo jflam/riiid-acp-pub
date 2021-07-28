00ade362df12be707575c0bb4de36b201600b985
John Lam <jlam@iunknown.com>
2021-07-28 12:20:44 -0700

    adding missing ipc-host flag

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -p 8888:8888 --mount type=bind,source="$(pwd)"/,target=/home/jlam/repo --rm -it jflam/kaggle
+docker run --privileged --ipc=host --gpus=all -p 8888:8888 --mount type=bind,source="$(pwd)"/,target=/home/jlam/repo --rm -it jflam/kaggle
