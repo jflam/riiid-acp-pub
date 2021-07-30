535a47c0ae7be86069237f32db6b7d02c6f930eb
jlam <jlam@iunknown.com>
2021-07-30 04:46:42 +0000

    updating run.sh to run as root for now

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --privileged --ipc=host --gpus=all -p 8888:8888 --mount type=bind,source="$(pwd)"/,target=/home/jlam/repo --rm -it jflam/kaggle
+docker run --ipc=host --gpus=all -p 8888:8888 --mount type=bind,source="$(pwd)"/,target=/root/repo --rm -it jflam/kaggle /bin/bash
